ed7e99936e20024c2918817d3393605b0456f14f
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-09-06 14:30:31 -0600

    initial commit.

---
+++ CoffeeFactorySimulation.cpp
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------
+Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+@author Sara Echeverria
+FileName: CoffeeFactorySimulation
+@version: I
+Creation: 06/09/2022
+Last modification: 06/09/2022
+------------------------------------------------------------------------------*/
+// Imports
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <math.h>
+#include <errno.h>
+
+// Namespace
+using namespace std;
